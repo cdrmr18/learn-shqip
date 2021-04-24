bfb4e4a3a37ca910c5f960eeefd85a2be0350524
Kathy Tavia <kathy.tavia@gmail.com>
2021-04-24 11:32:35 +0200

    deleted model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_083850) do
+ActiveRecord::Schema.define(version: 2021_04_24_092351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "phrases", force: :cascade do |t|
+    t.string "alb"
+    t.string "en"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "vocabularies", force: :cascade do |t|
     t.string "albanian"
